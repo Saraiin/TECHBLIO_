04e32498ad361b328c6c74ba71d613e13afcfef4
Sara <125512367+Saraiin@users.noreply.github.com>
2024-03-18 22:09:21 +0100

    add some changes

--- techblio/src/components/Navbar.jsx
+++ techblio/src/components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
     };
 
   return (
-    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
+    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white className="bg-yellow-100 dark:bg-blue-900">'>
       <h1 className='w-full text-3xl font-bold text-[#00df9a]'>TECHBLIO.</h1>
       <ul className='hidden md:flex'>
         <li className='p-4'>Home</li>
@@ -35,16 +35,16 @@ const Navbar = () => {
                     !dark && <IoMoon />
                 }
         </button>
-      <div onClick={handleNav} className='block md:hidden'>
+        <div onClick={handleNav} className='block md:hidden'>
           {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
-      </div>
-      <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
-        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>TECHBLIO.</h1>
+        </div>
+        <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
+          <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>TECHBLIO.</h1>
           <li className='p-4 border-b border-gray-600'>Home</li>
           <li className='p-4 border-b border-gray-600'>Resources</li>
           <li className='p-4 border-b border-gray-600'>About</li>
           <li className='p-4'>Contact</li>
-      </ul>
+        </ul>
     </div>
   );
 };
