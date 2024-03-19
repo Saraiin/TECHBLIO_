d173d6d044a062b266089cc4190870d876f2cc10
Sara <125512367+Saraiin@users.noreply.github.com>
2024-03-19 01:52:50 +0000

    add the welcoming page

--- techblio/src/App.js
+++ techblio/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Hero from './components/Hero';
+
 
 function App() {
   return (
     <div className="bg-gray-50 dark:bg-gray-600">
       <Navbar />
+      <Hero />
       <Footer />
       
     </div>
--- techblio/src/components/Hero.jsx
+++ techblio/src/components/Hero.jsx
@@ -15,13 +15,13 @@ const Hero = () => {
           <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4'>
             Ressources, e-books, Roadmaps for
           </p>
-          <Typed
+          {/*<Typed
           className='md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2'
             strings={['React', 'Javascript', 'Python']}
             typeSpeed={120}
             backSpeed={140}
             loop
-          />
+  />*/}
         </div>
         <p className='md:text-2xl text-xl font-bold text-gray-500'>Your tech knowledge hub.</p>
         <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>Get Started</button>
